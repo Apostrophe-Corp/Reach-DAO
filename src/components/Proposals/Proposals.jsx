7f35694627c4f6f0f35b901e53e7caaf305098dc
Your NameEmmanuel <agbavweisaac@gmail.com>
2022-08-29 00:55:20 +0100

    Update starting with the Proposals view

--- src/components/Proposals/Proposals.jsx
+++ src/components/Proposals/Proposals.jsx
@@ -5,6 +5,7 @@ import { useReach, fmtClasses } from "../../hooks";
 import styles from "../../styles/Shared.module.css";
 import Contribute from "./Contribute";
 import proposal from "../../styles/Proposals.module.css";
+import info from "../../styles/Info.module.css";
 
 const Proposals = () => {
     const { connectAndUpvote, connectAndDownvote, connectAndClaimRefund, proposals, setViews, standardUnit } = useReach();
@@ -193,6 +194,15 @@ const Proposals = () => {
             styles.directionY,
             styles.gap15,
         ) }>
+            <h1 className={ fmtClasses(info.devsText, styles.widthMax, styles.tCenter) }>Proposals</h1>
+            <div className={ fmtClasses(styles.widthMax) }>
+                <p className={ fmtClasses(
+                    styles.widthMax,
+                    styles.tCenter,
+                ) }>
+                    DApps are made of multiple agents interacting with each other through some backend consensus network, like Ethereum or Algorand. These agents act on behalf of principals that provide direction and authority through information. These principals might be humans or other autonomous agents or even committees and organizations with their own structure. The consensus network allows these agents to transfer and receive value in the form of network-specific tokens, like ETH or ALGO. The network also allows the creation of "contracts" that ensure that all agents follow the same rules as they take turns computing and publishing values and information. The details of these "contracts" are specific to each consensus network, but they are implicitly trusted by all agents and principals because their operation can be independently verified to match the previously agreed-upon rules.
+                </p>
+            </div>
             {
                 proposals.filter(el => el.id > ((page - 1) * 5) && el.id <= ((page) * 5)).map((el, i) => <Proposal el={ el } key={ i } />)
             }
