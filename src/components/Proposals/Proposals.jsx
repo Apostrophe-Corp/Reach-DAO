f73003b2c153b6309e74c229d466ff5faa386109
Your NameEmmanuel <agbavweisaac@gmail.com>
2022-08-29 01:00:48 +0100

    Update proposals

--- src/components/Proposals/Proposals.jsx
+++ src/components/Proposals/Proposals.jsx
@@ -200,7 +200,9 @@ const Proposals = () => {
                     styles.widthMax,
                     styles.tCenter,
                 ) }>
-                    DApps are made of multiple agents interacting with each other through some backend consensus network, like Ethereum or Algorand. These agents act on behalf of principals that provide direction and authority through information. These principals might be humans or other autonomous agents or even committees and organizations with their own structure. The consensus network allows these agents to transfer and receive value in the form of network-specific tokens, like ETH or ALGO. The network also allows the creation of "contracts" that ensure that all agents follow the same rules as they take turns computing and publishing values and information. The details of these "contracts" are specific to each consensus network, but they are implicitly trusted by all agents and principals because their operation can be independently verified to match the previously agreed-upon rules.
+                    Transform your ideas into products. Create a proposal, let your peers contribute and vote on it and if it passes it becomes a bounty.
+                    <br />
+                    Be it a full scale project, a part of one or a tutorial, tap into our pull of blockchain enthusiasts to build them with you.
                 </p>
             </div>
             {
